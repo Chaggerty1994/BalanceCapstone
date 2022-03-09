8f49d2c72665c9e34dc1b1e537b82db6bde89b10
Christian Haggerty <captivationstation@gmail.com>
2022-03-09 14:24:02 -0600

    register added

--- src/Components/Balance.js
+++ src/Components/Balance.js
@@ -1,9 +1,43 @@
-
 import React from "react";
+import { Route } from "react-router-dom";
+import { Redirect } from "react-router-dom";
+import { AppViews } from "../ApplicationViews";
+import { Register } from "../auth/register";
+import { Login } from "../auth/login";
 
-
+import { NavBar } from "../nav/NavBar";
 
 
 export const Balance = () => {
-    return <h1>BaL.Ance</h1>
+    return (
+        <>
+
+        <Route
+            render={() => {
+                if (localStorage.getItem("balance_user")) {
+                    return (
+                        <>
+                        <NavBar />
+                         
+                         <AppViews />
+                         </>
+                    );
+                } else {
+                    return <Redirect to="/login" />;
+                }
+            }}
+
+         />
+
+            <Route path="/login">
+                <Login />
+            </Route>
+
+            <Route path="/register">
+                <Register />
+            </Route>
+
+
+         </>
+    )
 }
