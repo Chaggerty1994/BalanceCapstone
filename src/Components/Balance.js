c802b8e00c1b4f759e798a0d6e68c77c09fe4833
Christian Haggerty <captivationstation@gmail.com>
2022-03-09 13:26:55 -0600

    first module made

---
+++ src/Components/Balance.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+import 
+
+
+export const Balance = () => {
+    return <header><img src={`Balance-Logo.PNG`} height={100} width={100} /></header>
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
 import reportWebVitals from './reportWebVitals';
+import { Balance } from './Components/Balance';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Balance />
   </React.StrictMode>,
   document.getElementById('root')
 );
