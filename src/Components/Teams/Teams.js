e8b54dc8aec7bf6944358abd2ac7856777111225
Christian Haggerty <captivationstation@gmail.com>
2022-03-23 10:08:21 -0500

    MyTeam Page

---
+++ src/Components/Teams/Teams.js
@@ -0,0 +1,152 @@
+import { Button, IconButton, Paper } from "@mui/material";
+import React, { useEffect, useState, getItem } from "react";
+import "./Teams.css"
+import EditIcon from '@mui/icons-material/Edit';
+import { useRouteMatch } from "react-router-dom";
+// im going to access only the currently signed in users information
+// display that information 
+// then make each property editable
+
+
+
+export const MyTeam = () => {
+
+    // defining a state variable and setter function for the users
+    const [users, setUsers] = useState([])
+    // doing a fetch call for an array of all the users in the API
+    useEffect(
+        () => {
+            fetch("http://localhost:8088/users")
+                .then(res => res.json())
+                .then((usersArray) => {
+                    setUsers(usersArray)
+                })
+        },
+        []
+    )
+
+    const [currentUser, setCurrentUser] = useState(0)
+
+
+    useEffect(
+        () => {
+            setCurrentUser(userId)
+        }, []
+    )
+        console.log(currentUser)
+ 
+    // assigning the value of the user in localstorage to a variable
+
+        const userId = localStorage.getItem("balance_user")
+        console.log(userId)
+
+
+    // // declaring a state variable for the specific property of the user that you are 
+    // // editing when you click to edit button. its initial
+    // // value is set to null because it wont be anything until
+    // // you select one to edit.  
+    // const [userNameEditing, setUserNameEditing] = useState(null)
+    // const [userEmailEditing, setUserEmailEditing] = useState(null)
+    // const [usersUserNameEditing, setUsersUserNameEditing] = useState(null)
+
+    // //declaring a state variable for the new text that will be replacing
+    // // the old text . its initial value is an empty string.
+
+    // const [editingUserName, setEditingUserName] = useState("")
+    // const [editingUserEmail, setEditingUserEmail] = useState("")
+    // const [editingUsersUserName, setEditingUsersUserName] = useState('')
+
+    const fetchUsers = () => {
+        fetch("http://localhost:8088/users")
+            .then(res => res.json())
+            .then((usersFromAPI) => {
+                setUsers(usersFromAPI)
+            })
+    }
+
+    // const changeUser = (userObject) => {
+    //     fetch(`http://localhost:8088/users/${userObject.id}`, {
+    //         method: "PUT",
+    //         headers: {
+    //             "Content-Type": "application/json"
+    //         },
+    //         body: JSON.stringify(userObject)
+    //     })
+    //         .then(res => res.json())
+    //         .then(fetchUsers)
+    // }
+        
+    // const editUserName = (userObject) => {
+     
+    //     const copy = { ...userObject }
+    //     copy.name = editingUserName
+    //     changeUser(copy)
+        
+    // }
+
+    // const editUserEmail = (userObject) => {
+     
+    //     const copy = { ...userObject }
+    //     copy.email = editingUserEmail
+    //     changeUser(copy)
+        
+    // }
+
+
+    // const editUsersUserName = (userObject) => {
+     
+    //     const copy = { ...userObject }
+    //     copy.userName = editingUsersUserName
+    //     changeUser(copy)
+        
+    // }
+
+    
+
+
+    return (
+        
+        
+        <>
+        <div className="account">
+        <Paper className="accountinfo"
+            elevation={12}
+            style={{
+                margin: "0px 0px 8px 0px",
+                border: "2px solid purple" 
+                }}>
+                    {
+                        currentUser && users ?    
+                        users.map (
+                            (user) => { 
+
+                                // need to figure out how to get the currently logged in user 
+                                // so i can compare id to the users being mapped
+                                if (user.Id === parseInt(currentUser)) {
+                                    return  <>
+                                    <div className="myteam">
+                                    <ul >
+                                    <h2 className="teamheader">MyTeam</h2>
+                                     
+
+                                    
+                                  
+                                   
+                                    
+                                    
+
+                                    </ul>
+                                    </div>
+                                         </>
+                                    
+                                          
+                                }
+                            }
+                        ) : "" 
+                    }
+       
+        </Paper >
+        </div>
+        </>
+    )
+}
